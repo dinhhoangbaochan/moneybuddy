fad5bb9d76912fd5f5f1fa71cd0de64990748760
Chan Dinh <dinhhoangbaochan@gmail.com>
2023-11-08 22:05:42 +0700

    poc/feat: initial crud to perform supabase test

--- app.vue
+++ app.vue
@@ -1,5 +1,11 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <!-- <NuxtWelcome /> -->
   </div>
 </template>
+
+<script setup>
+const client = useSupabaseClient()
+const { data: people } = await useAsyncData('people', async () => client.from('people').select('*').order('created_at'), { transform: result => result.data }); 
+console.log('heere: ', people);
+</script>
